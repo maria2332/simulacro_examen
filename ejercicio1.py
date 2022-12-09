617a679ca44f1bff863a3d78f03f058fe97bd8ef
maria2332 <maria.ballesteros2004@gmail.com>
2022-12-09 11:34:00 +0100

    cambio

--- ejercicio1.py
+++ ejercicio1.py
@@ -15,28 +15,37 @@ Tu tarea es determinar el ganador del juego y su puntuación. El nombre y la pun
 o Draw si no hay ningún ganador.
 """
 
-from ast import main
-
+# Definimos una función que tome una cadena de caracteres y devuelva un diccionario con las subcadenas y su puntuación
 def minion_game(string):
-    vowels = 'AEIOU'
-    kevin_score = 0
-    stuart_score = 0
-    for i in range(len(string)):
-        if [i] in vowels:
-            kevin_score += (len(string)-i)
-        else:
-            stuart_score += (len(string)-i)
-    if kevin_score > stuart_score:
-        print ("Kevin", kevin_score)
-    elif kevin_score < stuart_score:
-        print ("Stuart", stuart_score)
+  # Creamos un diccionario vacío que almacenará las subcadenas y su puntuación
+  substrings = {}
+  # Iteramos sobre todas las letras de la cadena
+  for i in range(len(string)):
+    # Stuart forma palabras que comiencen con consonantes
+    if string[i] not in "AEIOU":
+      # Iteramos sobre todas las subcadenas que comiencen con la letra actual
+      for j in range(i + 1, len(string) + 1):
+        # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
+        substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+        
+    # Kevin forma palabras que comiencen con vocales
     else:
-        print ("Draw")
-
-minion_game("BANANA")
-
-if kevin_word == startswith("AEIOU")
-
-
-if __name__ == '__main__':   
-    main()
+      # Iteramos sobre todas las subcadenas que comiencen con la letra actual
+      for j in range(i + 1, len(string) + 1):
+        # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
+        substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+  return substrings
+# Probamos la función con la cadena "BANANA"
+print(minion_game("BANANA"))
+# Deberíamos obtener el siguiente diccionario como resultado:
+# {
+#   "B": 1,
+#   "BA": 1,
+#   "BAN": 1,
+#   "BANA": 1,
+#   "AN": 2,
+#   "ANA": 2,
+#   "N": 2,
+#   "NA": 2,
+#   "A": 3
+# }
